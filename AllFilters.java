0243376088ec7825750f2036766df2dd4fa22c43
Shandy Kim <36430384+shandyk18@users.noreply.github.com>
2018-09-03 12:07:01 -0700

    Add javadocs to AllFilters

--- AllFilters.java
+++ AllFilters.java
@@ -1,16 +1,34 @@
 import java.util.ArrayList;
 
 public class AllFilters implements Filter {
+    /**
+    * List of movie Filters
+    */
     ArrayList<Filter> filters;
     
+    /**
+    * Constructor for AllFilters class
+    *
+    */
     public AllFilters() {
         filters = new ArrayList<Filter>();
     }
-
+    
+    /**
+    * Adds each filter to an array list so multiple filters may be used at once
+    *
+    * @param f a Filter given by user to use
+    */
     public void addFilter(Filter f) {
         filters.add(f);
     }
-
+    
+    /**
+    * Checks if movie satisfies criteria
+    *
+    * @param id String ID of movie
+    * @return returns a boolean of whether movie satisfies filter criteria
+    */
     @Override
     public boolean satisfies(String id) {
         for(Filter f : filters) {
@@ -18,8 +36,6 @@ public class AllFilters implements Filter {
                 return false;
             }
         }
-        
         return true;
     }
-
 }
